bf6260b3dd90cdbd4a0f25bf3acbd02941daf0c7
Billmetal <billmetal9@gmail.com>
2022-05-06 15:02:42 -0300

    Melhorado a lista de movimentações com a busca por correntista e inplementado o componente de correntistas .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http'
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 
 
 registerLocaleData(localePt);
@@ -20,7 +21,8 @@ registerLocaleData(localePt);
   declarations: [
     AppComponent,
     MovimentacaoListComponent,
-    MovimentacaoNewComponent
+    MovimentacaoNewComponent,
+    CorrentistaComponent
   ],
   imports: [
     BrowserModule,
